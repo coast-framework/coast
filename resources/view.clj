d8cb836f2d6d5d29dee1e1d84bc1210e649c0810
Sean Walker <swlkr.rbl@gmail.com>
2017-10-11 19:52:19 -0700

    Update view generator

--- resources/view.clj
+++ resources/view.clj
@@ -1,42 +1,45 @@
 (ns {{project}}.views.{{table}}
-  (:require [{{project}}.components :as c]))
+  (:require [{{project}}.components :as c]
+            [coast.core :as coast]))
 
-(defn {{singular}} [m]
+(defn row [{{singular}}]
   [:tr{% for col in columns %}
    [:td (:{{col}} m)]{% endfor %}
    [:td
-    [:a {:href (str "/{{table}}/" (:id m) "/edit")} "Edit"]]
+    (coast/link-to "Edit" :{{table}}/edit {{singular}})]
    [:td
-     [:a {:href (str "/{{table}}/" (:id m))} "Show"]]])
+    (coast/link-to "Show" :{{table}} {{singular}})]])
 
 (defn index [{{table}}]
   [:table
    (map {{singular}} {{table}})]
   [:div
-   [:a {:href "/{{table}}/new"} "New {{singular}}"]])
+   (coast/link-to "New {{singular}}" :{{table}}/new)])
 
 (defn show [{{singular}}]
  {% for col in columns %}
  [:div (:{{col}} {{singular}}{% endfor %})]
  [:div
-  [:a {:href "/{{table}}"} "Back"]])
+  (coast/link-to "Back" :{{table}})])
 
 (defn new- [{:keys [{{singular}} error]}]
-  error
-  (c/form {:action "/{{table}}" :method "post"}{% for col in form_columns %}
-    [:div
-      [:input {:type "text" :name "{{col}}" :value (:{{col}} {{singular}})}]{% endfor %}]
-    [:div
-      [:input {:type "submit" :value "Create"}]]
-    [:div
-      [:a {:href "/{{table}}"} "Back"]]))
+  [:div
+    error
+    (coast/form-for [:{{table}}] [{{singular}}]{% for col in form_columns %}
+      [:div
+        (coast/field {:type "text" :name "{{col}}" :value (:{{col}} {{singular}})}){% endfor %}]
+      [:div
+        [:input {:type "submit" :value "Save"}]]
+      [:div
+        (coast/link-to "Back" [:{{table}}])])])
 
 (defn edit [{:keys [{{singular}} error]}]
-  error
-  (c/form {:action (str "/{{table}}/" (:id {{singular}})) :method "put"}{% for col in form_columns %}
-    [:div
-     [:input {:type "text" :name "{{col}}" :value (:{{col}} {{singular}})}]{% endfor %}]
-    [:div
-     [:input {:type "submit" :value "Update"}]]
-    [:div
-     [:a {:href "/{{table}}"} "Back"]]))
+  [:div
+    error
+    (coast/form-for [:{{table}}] [{{singular}}]{% for col in form_columns %}
+      [:div
+       (coast/field {:type "text" :name "{{col}}" :value (:{{col}} {{singular}})}){% endfor %}]
+      [:div
+       [:input {:type "submit" :value "Save"}]]
+      [:div
+       (coast/link-to "Back" [:{{table}}])])])
