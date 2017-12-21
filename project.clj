2b5808022f6f54191b1a696255b9ba88e03ade59
Sean Walker <swlkr.rbl@gmail.com>
2017-12-20 23:01:34 -0700

    Bump trail

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [potemkin "0.4.4"]
                  [http-kit "2.2.0"]
-                 [trail "2.0.0"]
+                 [trail "2.0.1"]
                  [trek "0.1.4"]
                  [bunyan "0.1.1"]
                  [environ "1.1.0"]
