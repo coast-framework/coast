ad34c9397bb5e20ff91886aae38ecbc56cc82a2d
Sean Walker <swlkr.rbl@gmail.com>
2017-12-20 23:12:33 -0700

    Make sure view works

--- resources/view.clj
+++ resources/view.clj
@@ -3,7 +3,7 @@
             [coast.core :as coast]))
 
 (defn {{singular}} [m]
-  (let [{:keys [{{column_string}}] m}]
+  (let [{:keys [{{column_string}}]} m]
     [:tr{% for col in columns %}
      [:td {{col}}{% endfor %}]
      [:td
