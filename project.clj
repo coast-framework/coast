917ded621b46a6e015ea47b2018f10ba23c9095b
Sean Walker <swlkr.rbl@gmail.com>
2018-02-25 12:34:18 -0800

    Rename coast -> coast.alpha

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject coast "0.6.8"
+(defproject coast.alpha "0.1.0"
   :description "An easy full stack framework for clojure"
   :url "https://coastonclojure.com"
   :license {:name "MIT License"
