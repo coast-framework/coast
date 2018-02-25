78cca5b2d9a85e8d23caef29b01cb6ccc53050f6
Sean Walker <swlkr.rbl@gmail.com>
2018-02-25 15:30:45 -0800

    Update db generator to reference db.sql files

--- resources/generators/db.clj
+++ resources/generators/db.clj
@@ -2,8 +2,8 @@
   (:require [coast.alpha.db :refer [defq]])
   (:refer-clojure :exclude [update list find]))
 
-(defq list "resources/sql/__table.model.sql")
-(defq find "resources/sql/__table.model.sql")
-(defq insert "resources/sql/__table.model.sql")
-(defq update "resources/sql/__table.model.sql")
-(defq delete "resources/sql/__table.model.sql")
+(defq list "resources/sql/__table.db.sql")
+(defq find "resources/sql/__table.db.sql")
+(defq insert "resources/sql/__table.db.sql")
+(defq update "resources/sql/__table.db.sql")
+(defq delete "resources/sql/__table.db.sql")
