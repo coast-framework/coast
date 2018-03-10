c123f2b1642427d0cd0806efc98d10a5ec053776
Sean Walker <swlkr.rbl@gmail.com>
2018-03-10 09:23:40 -0800

    Update generators

--- src/coast/generators.clj
+++ src/coast/generators.clj
@@ -3,7 +3,8 @@
             [coast.db :as db]
             [coast.utils :as utils]
             [coast.words :as words]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [coast.migrations :as migrations]))
 
 (def pattern #"__([\w-]+)")
 
@@ -20,7 +21,7 @@
 (defn excluded-cols [s]
   (not (contains? excluded-col-set s)))
 
-(defn sql [_ table]
+(defn sql [table]
   (let [cols (->> (db/columns {:table-name table})
                   (map :column-name)
                   (filter excluded-cols))
@@ -39,21 +40,21 @@
          (spit output-filename))
     (println (format "%s created successfully" output-filename))))
 
-(defn db [project table]
-  (let [output (format "src/%s/db/%s.clj" project table)]
+(defn db [table]
+  (let [output (format "src/db/%s.clj" table)]
     (->> (io/resource "generators/db.clj")
          (slurp)
-         (fill {:table (utils/kebab table)
-                :project (utils/kebab project)})
+         (fill {:table (utils/kebab table)})
          (spit output))
     (println (format "%s created successfully" output))))
 
-(defn model [project table]
-  (let [output (format "src/%s/models/%s.clj" project table)]
+(defn model [table]
+  (let [output (format "src/models/%s.clj" table)
+        _ (sql table)
+        _ (db table)]
     (->> (io/resource "generators/model.clj")
          (slurp)
          (fill {:table (utils/kebab table)
-                :project (utils/kebab project)
                 :singular (-> table utils/kebab words/singular)
                 :columns (->> (db/columns {:table-name table})
                               (map :column-name)
@@ -62,17 +63,16 @@
          (spit output))
     (println (format "%s created successfully" output))))
 
-(defn controller [project table]
-  (let [output (format "src/%s/controllers/%s.clj" project table)]
+(defn controller [table]
+  (let [output (format "src/controllers/%s.clj" table)]
     (->> (io/resource "generators/controller.clj")
          (slurp)
          (fill {:table (utils/kebab table)
-                :project (utils/kebab project)
                 :singular (-> table utils/kebab words/singular)})
          (spit output))
     (println (format "%s created successfully" output))))
 
-(defn view [project table]
+(defn view [table]
   (let [columns (->> (db/columns {:table-name table})
                      (map :column-name)
                      (filter excluded-cols)
@@ -85,11 +85,10 @@
                          (string/join "\n      "))
         form-columns (->> (map #(format "[:div\n         [:label \"%s\"]\n         [:input {:type \"text\" :name  \"%s\" :value %s}]]" % % %) columns)
                           (string/join "\n       "))
-        output (format "src/%s/views/%s.clj" project table)]
+        output (format "src/views/%s.clj" table)]
     (->> (io/resource "generators/view.clj")
          (slurp)
-         (fill {:project (utils/kebab project)
-                :table (utils/kebab table)
+         (fill {:table (utils/kebab table)
                 :singular (words/singular table)
                 :columns (string/join " " columns)
                 :td-columns td-columns
@@ -98,3 +97,45 @@
                 :div-columns div-columns})
          (spit output))
     (println (format "%s created successfully" output))))
+
+(defn usage []
+  (println "Usage:
+  coast new <project-name>
+
+Examples:
+  coast new foo
+  coast new another-foo
+
+  coast gen migration                                          # Creates a new migration
+  coast gen migration create-<table> column1:text column2:text # Creates a new migration with a create table statement along with columns/types specified
+  coast gen sql <table>                                        # Creates a new db.sql file in resources/sql with default sql queries/statement
+  coast gen db <table>                                         # Creates a new db.clj file in src/db with default database functions
+  coast gen model <table>                                      # Creates a new model.clj file in src/models
+  coast gen controller <table>                                 # Creates a new controller.clj file in src/controllers
+  coast gen view <table>                                       # Creates a new view.clj file in src/views"))
+
+(defn gen [args]
+  (let [[_ kind table] args]
+    (case kind
+      "migration" (apply migrations/create (drop 2 args))
+      "sql" (sql table)
+      "db" (db table)
+      "model" (do
+                (sql table)
+                (db table)
+                (model table))
+      "controller" (controller table)
+      "view" (view table)
+      "mvc" (do
+              (sql table)
+              (db table)
+              (model table)
+              (controller table)
+              (view table))
+      (usage))))
+
+(defn -main [& args]
+  (let [[action] args]
+    (case action
+      "gen" (gen args)
+      (usage))))
