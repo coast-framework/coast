9cd39ebabd40e3c94d9aa2c7fd9b7b374bc68eaa
Sean Walker <swlkr.rbl@gmail.com>
2018-05-17 08:32:18 -0700

    Thread last in prod.server

--- src/coast/prod/server.clj
+++ src/coast/prod/server.clj
@@ -5,9 +5,9 @@
 
 (defn start
   ([app port]
-   (let [opts (-> (or port (env/env :port) "1337")
-                  (utils/parse-int)
-                  (hash-map :port))]
+   (let [opts (->> (or port (env/env :port) "1337")
+                   (utils/parse-int)
+                   (hash-map :port))]
      (println "Server is listening on port" (:port opts))
      (httpkit/run-server app opts)))
   ([app]
