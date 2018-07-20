e84a3e86401d1ef968de3932a4b6574ebe0338ee
Sean Walker <swlkr.rbl@gmail.com>
2018-07-20 09:49:27 -0700

    Rescue from specific errors

--- src/coast/error.clj
+++ src/coast/error.clj
@@ -7,16 +7,20 @@
   ([m]
    (raise "Error has occurred" m)))
 
-(defmacro rescue [f]
-  `(try
-    [~f nil]
-    (catch org.postgresql.util.PSQLException e#
-      (let [error-map# (db.errors/error-map e#)]
-        (if (empty? error-map#)
-          (throw e#)
-          [nil error-map#])))
-    (catch clojure.lang.ExceptionInfo e#
-      (let [ex# (ex-data e#)]
-        (if (contains? ex# ::raise)
-          [nil ex#]
-          (throw e#))))))
+(defmacro rescue
+  ([f k]
+   `(try
+     [~f nil]
+     (catch org.postgresql.util.PSQLException e#
+       (let [error-map# (db.errors/error-map e#)]
+         (if (empty? error-map#)
+           (throw e#)
+           [nil error-map#])))
+     (catch clojure.lang.ExceptionInfo e#
+       (let [ex# (ex-data e#)]
+         (if (and (contains? ex# ::raise)
+                  (contains? ex# (or ~k ::raise)))
+           [nil ex#]
+           (throw e#))))))
+  ([f]
+   `(rescue ~f nil)))
