47525624e34ee683d82ceb624bffae5479066fa6
Sean Walker <swlkr@fastmail.com>
2018-08-08 10:36:33 -0700

    Read .nrepl-port file when starting nrepl

--- bin/repl.clj
+++ bin/repl.clj
@@ -1,8 +1,9 @@
 (ns repl
   (:require [nrepl.server]))
 
-(let [port (or (some-> (first *command-line-args*)
-                       (java.lang.Long/parseLong))
+(let [port (or (some->> (slurp ".nrepl-port")
+                        (re-find #"^\d+$")
+                        (Integer.))
                7888)]
   (nrepl.server/start-server :port port)
   (println "Started nREPL on port" port))
