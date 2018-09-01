47263e0fe17ac6ef88e8adc98d5b5210282058b1
Sean Walker <swlkr@fastmail.com>
2018-09-01 08:02:19 -0700

    Get rid of sql migration generators

--- src/coast/migrations/sql.clj
+++ src/coast/migrations/sql.clj
@@ -38,81 +38,9 @@
           (string? name))
     (str (timestamp) "_" (string/replace name #"\s+|-+|_+" "_") ".sql")))
 
-(defn column [arg]
-  (let [[name type] (string/split arg #":")]
-    (str name " " type)))
-
-(defn column-name [arg]
-  (-> arg
-      (string/split #":")
-      (first)))
-
-(defn parse-table-name [s]
-  (->> (string/split s #"[-_]")
-       (drop 1)
-       (string/join "_")))
-
-(defn create-table-contents [name args]
-  (let [table (parse-table-name name)
-        sql (str "create table " table " (\n")
-        columns (map column args)
-        columns (-> columns
-                    (conj "id serial primary key")
-                    vec
-                    (conj "created_at timestamptz default now()"))
-        column-string (string/join ",\n  " columns)]
-    (str sql column-string "\n)")))
-
-(defn drop-table-contents [s]
-  (str "drop table " (parse-table-name s)))
-
-(defn add-column-contents [s args]
-  (let [table (->> (re-seq #"add-columns-to-([\w-_]+)" s)
-                   (first)
-                   (last))
-        columns (map column args)]
-    (->> (map #(str "alter table " table  " add column " % ";") columns)
-         (string/join "\n"))))
-
-(defn add-column-contents-down [s args]
-  (let [table (->> (re-seq #"add-columns-to-([\w-_]+)" s)
-                   (first)
-                   (last))
-        columns (map column args)]
-    (->> (map #(str "alter table " table  " drop " % ";") columns)
-         (string/join "\n"))))
-
-(defn drop-column-contents [s args]
-  (let [table (->> (re-seq #"drop-columns-from-([\w-_]+)" s)
-                   (first)
-                   (last))
-        columns (map column-name args)]
-    (->> (map #(str "alter table " table  " drop " % ";") columns)
-         (string/join "\n"))))
-
-(defn drop-column-contents-down [s args]
-  (let [table (->> (re-seq #"drop-columns-from-([\w-_]+)" s)
-                   (first)
-                   (last))
-        columns (map column-name args)]
-    (->> (map #(str "alter table " table  " add column " % ";") columns)
-         (string/join "\n"))))
-
-(defn contents [s args]
-  (cond
-    (string/starts-with? s "create-") (str "-- up\n" (create-table-contents s args) "\n\n"
-                                           "-- down\n" (drop-table-contents s))
-    (string/starts-with? s "drop-") (str "-- up\n\n"
-                                         "-- down\n" (drop-table-contents s))
-    (string/starts-with? s "add-columns-to-") (str "-- up\n" (add-column-contents s args) "\n\n"
-                                                   "-- down\n" (add-column-contents-down s args))
-    (string/starts-with? s "drop-columns-from-") (str "-- up\n" (drop-column-contents s args) "\n\n"
-                                                      "-- down\n" (drop-column-contents-down s args))
-    :else empty-migration))
-
-(defn create [name & args]
+(defn create [name]
   (let [migration (filename name)
         dir (migrations-dir)]
-    (spit (str dir "/" migration) (contents name args))
+    (spit (str dir "/" migration) "-- up\n\n-- down")
     (println (str dir "/" migration " created"))
     migration))
