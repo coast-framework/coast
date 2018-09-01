c2018a8cb6220dad30f823d35fdb4364ec54f4a8
Sean Walker <swlkr@fastmail.com>
2018-09-01 08:05:15 -0700

    Add src/coast and move zeta -> eta

---
+++ src/coast.clj
@@ -0,0 +1,49 @@
+(ns coast
+  (:require [potemkin :refer [import-vars]]
+            [coast.db]
+            [coast.eta]
+            [coast.components]
+            [coast.responses]
+            [coast.utils]
+            [coast.error]
+            [coast.router]
+            [coast.validation]
+            [coast.middleware]))
+
+(import-vars
+  [coast.responses
+   ok
+   not-found
+   unauthorized
+   internal-server-error
+   redirect
+   flash]
+
+  [coast.error
+   raise
+   rescue]
+
+  [coast.db
+   q
+   pull
+   transact
+   first!]
+
+  [coast.validation
+   validate]
+
+  [coast.components
+   form
+   js
+   css]
+
+  [coast.middleware
+   wrap-layout]
+
+  [coast.router
+   action-for
+   url-for]
+
+  [coast.eta
+   server
+   app])
--- src/coast/eta.clj
+++ src/coast/eta.clj
@@ -1,10 +1,10 @@
-(ns coast.zeta
+(ns coast.eta
   (:require [coast.middleware :as middleware]
             [coast.router :as router]
             [coast.dev.server :as dev.server]
             [coast.prod.server :as prod.server]
             [coast.env :refer [env]]
-            [coast.assets :as assets]
+            [clojure.java.io :as io]
             [ring.middleware.defaults :as middleware.defaults]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.content-type :refer [wrap-content-type]]
@@ -13,23 +13,29 @@
 (defn resolve-routes
   "Eager require route namespaces when app is called for uberjar compat"
   [routes]
-  (->> (map #(nth % 2) routes)
-       (map #(if (vector? %) (first %) %))
-       (map namespace)
-       (distinct)
-       (filter some?)
-       (map symbol)
-       (apply require)))
+  (when (contains? #{"test" "prod"} (env :coast-env))
+    (->> (map #(nth % 2) routes)
+         (map #(if (vector? %) (first %) %))
+         (map namespace)
+         (distinct)
+         (filter some?)
+         (map symbol)
+         (apply require))))
+
+(defn resolve-layout
+  "Eager require components"
+  []
+  (when (contains? #{"test" "prod"} (env :coast-env))
+    (require 'components)))
 
 (defn app
   "Tasteful ring middleware so you don't have to think about it"
   ([routes opts]
    ; hack for uberjar route resolution
    (resolve-routes routes)
-   (let [layout (get opts :layout)
-         not-found-page (get opts :404)
-         error-page (get opts :500)
-         bundles (when (contains? opts :assets) (assets/bundles (= "prod" (env :coast-env)) (:assets opts)))]
+   (let [layout (get opts :layout (resolve `components/layout))
+         not-found-page (get opts :404 (resolve `error.400/view))
+         error-page (get opts :500 (resolve `error.500/view))]
      (-> (router/handler not-found-page)
          (middleware/wrap-layout layout)
          (middleware/wrap-with-logger)
@@ -37,7 +43,6 @@
          (middleware/wrap-coerce-params)
          (router/wrap-route-info routes)
          (middleware/wrap-storage (get opts :storage))
-         (middleware/wrap-bundles bundles)
          (wrap-content-type)
          (wrap-not-modified)
          (wrap-keyword-params {:keywordize? true :parse-namespaces? true})
@@ -45,10 +50,12 @@
          (middleware/wrap-not-found not-found-page)
          (middleware/wrap-errors error-page))))
   ([routes]
-   (app routes {})))
+   (app routes {}))
+  ([]
+   (app router/routes)))
 
 (defn server
-  "Runs http-kit server based on COAST_ENV env variable, options are COAST_ENV=dev or COAST_ENV=prod"
+  "Runs an http-kit server based on the COAST_ENV env variable, options are dev, test or prod"
   ([app]
    (server app nil))
   ([app opts]
