c99bc33be9bbe321545ed7094fba9fc9f1a3ac85
Sean Walker <swlkr@fastmail.com>
2018-09-10 22:07:24 -0700

    Simplify job queuing

--- src/coast/jobs.clj
+++ src/coast/jobs.clj
@@ -9,10 +9,12 @@
 (defq queued "sql/jobs.sql")
 
 (defn qualified-name [var]
-  (let [{:keys [ns name]} (meta var)]
-    (format "%s/%s" (ns-name ns) name)))
+  (if (qualified-symbol? var)
+    (str var)
+    (let [{:keys [ns name]} (meta var)]
+      (format "%s/%s" (ns-name ns) name))))
 
-(defn queue
+(defn queue-job
   ([f args scheduled-at]
    (let [params {:function (qualified-name f)
                  :args (str args)
@@ -20,7 +22,12 @@
                  :scheduled-at scheduled-at}]
      (insert params)))
   ([f args]
-   (queue f args nil)))
+   (queue-job f args nil)))
+
+(defmacro queue [body]
+  `(let [f# (symbol (str *ns* "/" (-> '~body first symbol)))
+         args# (-> '~body rest)]
+     (queue-job f# args#)))
 
 (defn resolve* [s]
   (let [[q-ns q-name] (->> (string/split s #"/")
@@ -31,9 +38,12 @@
 (defn run [job]
   (let [{:keys [function args]} job
         f (resolve* function)
-        parsed-args (clojure.edn/read-string args)]
+        parsed-args (clojure.edn/read-string args)
+        parsed-args (if (sequential? parsed-args)
+                     parsed-args
+                     [parsed-args])]
     (when (not (nil? f))
-      (if (nil? parsed-args)
+      (if (empty? parsed-args)
           (f)
-          (f parsed-args))
+          (apply f parsed-args))
       (update (assoc job :finished-at (time/now))))))
