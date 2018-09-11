6a2cf585afc2ba9f2e6b4eaf6b3290c4732cf44b
Sean Walker <swlkr@fastmail.com>
2018-09-10 22:09:13 -0700

    Drop edn routing, clean up eta ns

--- src/coast/eta.clj
+++ src/coast/eta.clj
@@ -5,10 +5,7 @@
             [coast.prod.server :as prod.server]
             [coast.env :refer [env]]
             [clojure.java.io :as io]
-            [ring.middleware.defaults :as middleware.defaults]
-            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
-            [ring.middleware.content-type :refer [wrap-content-type]]
-            [ring.middleware.not-modified :refer [wrap-not-modified]]))
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]))
 
 (defn resolve-routes
   "Eager require route namespaces when app is called for uberjar compat"
@@ -22,37 +19,39 @@
          (map symbol)
          (apply require))))
 
-(defn resolve-layout
+(defn resolve-components
   "Eager require components"
-  []
-  (when (contains? #{"test" "prod"} (env :coast-env))
+  [opts]
+  (when (and (contains? #{"test" "prod"} (env :coast-env))
+             (not= :api (:wrap-defaults opts)))
     (require 'components)))
 
 (defn app
   "Tasteful ring middleware so you don't have to think about it"
   ([routes opts]
+   ; hack for url-for and action-for
+   (def routes routes)
    ; hack for uberjar route resolution
    (resolve-routes routes)
+   (resolve-components opts)
    (let [layout (get opts :layout (resolve `components/layout))
-         not-found-page (get opts :404 (resolve `error.400/view))
-         error-page (get opts :500 (resolve `error.500/view))]
+         not-found-page (get opts :404 (resolve `error.not-found/view))
+         error-page (get opts :500 (resolve `error.internal-server-error/view))]
      (-> (router/handler not-found-page)
          (middleware/wrap-layout layout)
          (middleware/wrap-with-logger)
-         (middleware/wrap-route-middleware)
-         (middleware/wrap-coerce-params)
-         (router/wrap-route-info routes)
          (middleware/wrap-storage (get opts :storage))
-         (wrap-content-type)
-         (wrap-not-modified)
+         (middleware/wrap-coerce-params)
          (wrap-keyword-params {:keywordize? true :parse-namespaces? true})
-         (middleware.defaults/wrap-defaults (middleware/coast-defaults opts))
+         (middleware/wrap-defaults opts)
+         (middleware/wrap-json-params opts)
+         (middleware/wrap-json-response opts)
+         (middleware/wrap-route-middleware)
+         (router/wrap-route-info routes)
          (middleware/wrap-not-found not-found-page)
          (middleware/wrap-errors error-page))))
   ([routes]
-   (app routes {}))
-  ([]
-   (app router/routes)))
+   (app routes {})))
 
 (defn server
   "Runs an http-kit server based on the COAST_ENV env variable, options are dev, test or prod"
@@ -62,3 +61,9 @@
    (if (= "prod" (env :coast-env))
      (prod.server/start app opts)
      (dev.server/restart app opts))))
+
+(defn url-for [k]
+  ((router/url-for-routes routes) k))
+
+(defn action-for [k]
+  ((router/action-for-routes routes) k))
