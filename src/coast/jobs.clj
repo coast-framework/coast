d2bd4226d62164644a0043df5208b69db40d86ec
Sean Walker <swlkr@fastmail.com>
2018-09-25 10:43:39 -0700

    Change jobs/queue to take a fn name and & args

--- src/coast/jobs.clj
+++ src/coast/jobs.clj
@@ -1,33 +1,31 @@
 (ns coast.jobs
   (:require [coast.time :as time]
+            [coast.db :refer [defq]]
             [clojure.string :as string]
-            [coast.db :refer [defq]])
+            [clojure.edn :as edn])
   (:refer-clojure :exclude [run update]))
 
 (defq insert "sql/jobs.sql")
 (defq update "sql/jobs.sql")
 (defq queued "sql/jobs.sql")
 
-(defn qualified-name [var]
-  (if (qualified-symbol? var)
-    (str var)
-    (let [{:keys [ns name]} (meta var)]
-      (format "%s/%s" (ns-name ns) name))))
+(defn qualified-symbol [val]
+  (when (qualified-ident? val)
+    (if (qualified-keyword? val)
+      (symbol (namespace val) (name val))
+      val)))
 
-(defn queue-job
-  ([f args scheduled-at]
-   (let [params {:function (qualified-name f)
-                 :args (str args)
-                 :finished-at nil
-                 :scheduled-at scheduled-at}]
-     (insert params)))
-  ([f args]
-   (queue-job f args nil)))
+(defn queue-job [f args scheduled-at]
+  (insert {:function (str (qualified-symbol f))
+           :args (str args)
+           :finished-at nil
+           :scheduled-at scheduled-at}))
 
-(defmacro queue [body]
-  `(let [f# (symbol (str *ns* "/" (-> '~body first symbol)))
-         args# (-> '~body rest)]
-     (queue-job f# args#)))
+(defn queue-at [at f & args]
+  (queue-job f args at))
+
+(defn queue [f & args]
+  (queue-job f args nil))
 
 (defn resolve* [s]
   (let [[q-ns q-name] (->> (string/split s #"/")
@@ -38,7 +36,7 @@
 (defn run [job]
   (let [{:keys [function args]} job
         f (resolve* function)
-        parsed-args (clojure.edn/read-string args)
+        parsed-args (edn/read-string args)
         parsed-args (if (sequential? parsed-args)
                      parsed-args
                      [parsed-args])]
@@ -47,3 +45,17 @@
           (f)
           (apply f parsed-args))
       (update (assoc job :finished-at (time/now))))))
+
+(defn run-jobs [jobs]
+  (doseq [j jobs]
+    (run j)))
+
+(defn start []
+  (-> (queued)
+      (run-jobs))
+  (Thread/sleep 10000)
+  (start))
+
+(defn -main [& args]
+  (println "The jobs table is currently being polled")
+  (start))
