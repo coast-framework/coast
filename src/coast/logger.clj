e39c1b5dc842af321f5ebd8e0c752d1aa25ea7ce
Sean Walker <swlkr.rbl@gmail.com>
2019-07-10 14:52:59 -0600

    Simplify logging by using epoch

--- src/coast/logger.clj
+++ src/coast/logger.clj
@@ -1,28 +1,27 @@
 (ns coast.logger
   (:require [clojure.string :as string]
             [coast.time :as time]
-            [coast.utils :as utils])
-  (:import (java.time Duration)))
+            [coast.utils :as utils]))
 
-(defn diff [start end]
-  (let [duration (Duration/between start end)]
-    (.toMillis duration)))
-
-(defn req-method [request]
-  (or (-> request :params :_method)
-      (:request-method request)))
 
 (defn log-str [request response start-time]
-  (let [ms (diff start-time (time/now))
+  (let [ms (- (time/now-millis) start-time)
         uri (:uri request)
         status (:status response)
-        method (-> (req-method request) name string/upper-case)
-        headers (->> (:headers response)
-                     (utils/map-vals string/lower-case))
+        route (:route response)
+        method (some-> request :request-method name string/upper-case)
+        headers (some->> (:headers response)
+                         (utils/map-keys string/lower-case))
         content-type (get headers "content-type")
-        route (:coast.router/name response)
-        timestamp (time/fmt (time/offset) "yyyy-MM-dd HH:mm:ss xx")]
-    (str timestamp " " method " \"" uri "\" " route " " status " " content-type " " ms "ms")))
+        timestamp (-> (time/now) (time/zoned) (time/fmt "yyyy-MM-dd HH:mm:ss xx"))]
+    (->> [timestamp method (str "\"" uri "\"")
+          route status content-type
+          ms "ms"]
+         (filter some?)
+         (map str)
+         (filter #(not (string/blank? %)))
+         (string/join " "))))
+
 
 (defn log [request response start-time]
   (println (log-str request response start-time)))
