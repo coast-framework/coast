6cc2420139aba0ce8672af820df8f6d5264f630a
Sean Walker <swlkr.rbl@gmail.com>
2019-07-10 14:54:27 -0600

    Add doctype fn to components

--- src/coast/components.clj
+++ src/coast/components.clj
@@ -1,7 +1,36 @@
 (ns coast.components
   (:require [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
-            [coast.env :refer [env]]
-            [coast.assets :as assets]))
+            [coast.assets :as assets]
+            [coast.env :as env]
+            [coast.router :as router]
+            [hiccup.page]))
+
+
+(defn doctype [k]
+  (get hiccup.page/doctype k))
+
+
+(defn css
+  ([req bundle opts]
+   (let [files (assets/bundle (env/env :coast-env) bundle)]
+     (for [href files]
+       [:link (merge {:href href :type "text/css" :rel "stylesheet"} opts)])))
+  ([req bundle]
+   (css nil bundle {}))
+  ([bundle]
+   (css nil bundle)))
+
+
+(defn js
+  ([req bundle opts]
+   (let [files (assets/bundle (env/env :coast-env) bundle)]
+     (for [src files]
+      [:script (merge {:src src :type "application/javascript"} opts)])))
+  ([req bundle]
+   (js nil bundle {}))
+  ([bundle]
+   (js nil bundle)))
+
 
 (defn csrf
   ([attrs]
@@ -11,29 +40,15 @@
   ([]
    (csrf {})))
 
+
+(defn simulated-method? [method]
+  (contains? #{:patch :put :delete} method))
+
+
 (defn form [params & body]
-  [:form (dissoc params :_method)
-   (csrf)
-   (when (contains? #{:patch :put :delete} (:_method params))
-     [:input {:type "hidden" :name "_method" :value (:_method params)}])
-   body])
-
-(defn css
-  ([req bundle opts]
-   (let [files (assets/bundle (env :coast-env) bundle)]
-     (for [href files]
-       [:link (merge {:href href :type "text/css" :rel "stylesheet"} opts)])))
-  ([req bundle]
-   (css nil bundle {}))
-  ([bundle]
-   (css nil bundle)))
-
-(defn js
-  ([req bundle opts]
-   (let [files (assets/bundle (env :coast-env) bundle)]
-     (for [src files]
-      [:script (merge {:src src :type "application/javascript"} opts)])))
-  ([req bundle]
-   (js nil bundle {}))
-  ([bundle]
-   (js nil bundle)))
+  (let [{:keys [_method]} params]
+    [:form (dissoc params :_method)
+     (csrf)
+     (when (simulated-method? _method)
+       [:input {:type "hidden" :name "_method" :value _method}])
+     body]))
