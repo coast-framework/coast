a668528b5e8b6b21bdf077e4e241e4c7066cf124
Daw-Ran Liou <dawran6@gmail.com>
2019-08-29 12:55:23 -0700

    Add docs for coast.components

--- src/coast/components.clj
+++ src/coast/components.clj
@@ -4,14 +4,18 @@
             [coast.assets :as assets]))
 
 (defn csrf
+  "Return a hidden input with the csrf token."
   ([attrs]
-   [:input (assoc attrs :type "hidden"
-                        :name "__anti-forgery-token"
-                        :value *anti-forgery-token*)])
+   [:input (assoc attrs
+                  :type "hidden"
+                  :name "__anti-forgery-token"
+                  :value *anti-forgery-token*)])
   ([]
    (csrf {})))
 
-(defn form [params & body]
+(defn form
+  "Return a form with the hidden input already added to the body"
+  [params & body]
   [:form (dissoc params :_method)
    (csrf)
    (when (contains? #{:patch :put :delete} (:_method params))
@@ -19,6 +23,23 @@
    body])
 
 (defn css
+  "Adds a link tag to a CSS bundle.
+
+  Relative path (to CSS files in the public directory):
+
+  ```clojure
+  (coast/css \"bundle.css\")
+
+  ; assuming the assets.edn looks something like this
+  {\"bundle.css\" [\"style.css\"]}
+  ```
+
+  The code above outputs:
+
+  ```html
+  <link rel=\"stylesheet\" href=\"/style.css\" />
+  ```
+  "
   ([req bundle opts]
    (let [files (assets/bundle (env :coast-env) bundle)]
      (for [href files]
@@ -29,10 +50,26 @@
    (css nil bundle)))
 
 (defn js
+  "Adds a script tag to a JS bundle.
+
+  ```clojure
+  (coast/js \"bundle.js\")
+
+  ; assuming the assets.edn looks something like this
+  {\"bundle.js\" [\"app.js\" \"app2.js\"]}
+  ```
+
+  The code above outputs:
+
+  ```html
+  <script type=\"text/javascript\" src=\"/app.js\" />
+  <script type=\"text/javascript\" src=\"/app2.js\" />
+  ```
+  "
   ([req bundle opts]
    (let [files (assets/bundle (env :coast-env) bundle)]
      (for [src files]
-      [:script (merge {:src src :type "application/javascript"} opts)])))
+       [:script (merge {:src src :type "application/javascript"} opts)])))
   ([req bundle]
    (js nil bundle {}))
   ([bundle]
