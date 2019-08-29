9d7177fa073077d664f52d20120ad9d5eef50920
Daw-Ran Liou <dawran6@gmail.com>
2019-08-29 09:48:46 -0700

    Add docs for coast.validation

--- src/coast/validation.clj
+++ src/coast/validation.clj
@@ -14,12 +14,35 @@
          (mapcat identity)
          (into {}))))
 
-(defn validate [m validations]
+(defn validate
+  "Validate the map `m` with a vector of rules `validations`.
+
+  For example:
+  ```
+  (validate {:customer/id 123
+             :customer/email \"sean@example.com\"}
+            [[:required [:customer/id :customer/email]]
+             [:email [:customer/email]]])
+  ;; => {:customer/id 123
+         :customer/email \"sean@example.com\"}
+
+  (validate {} [[:required [:customer/id] \"can't be blank\"]])
+  ;; => Unhandled clojure.lang.ExceptionInfo
+  ;;    Invalid data: :customer/id
+  ;;    {:type :invalid,
+  ;;     :errors #:customer{:id \"Id can't be blank\"},
+  ;;     :coast.validation/error :validation,
+  ;;     :coast.error/raise true}
+  ```
+
+  See [Validator](https://coastonclojure.com/docs/validator.md) for more.
+  "
+  [m validations]
   (let [errors (-> (v/validate m validations)
                    (fmt-validations))]
     (if (empty? errors)
       m
       (raise (str "Invalid data: " (string/join ", " (keys errors)))
-             {:type :invalid
+             {:type   :invalid
               :errors errors
               ::error :validation}))))
